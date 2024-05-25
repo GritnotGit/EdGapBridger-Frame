8ea98bb89ea09cdf77f20635787dcea1075a1200
GritnotGit <no.grit.no.pearl@icloud.com>
2024-05-25 15:22:22 +0200

    Updated page.tsx and config.ts for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from './config';
+import { NEXT_PUBLIC_URL } from './config'; // Der Import der Konfiguration
 
 const frameMetadata = getFrameMetadata({
   buttons: [
@@ -15,10 +15,9 @@ const frameMetadata = getFrameMetadata({
       target: `https://zora.co/collect/base:0xb1031d5f7e02de0481ec1c2743409fb6534401e9`,
     },
     {
-      // action: '',
-      label: 'mint to donate',
-      // target: `${NEXT_PUBLIC_URL}/api/tx`,
-      
+      action: 'link',
+      label: 'Donation',
+      target: `https://fv.sophie-scholl-schule.eu/verein/wie-kann-ich-spenden`,
     },
   ],
   image: {
@@ -33,10 +32,11 @@ const frameMetadata = getFrameMetadata({
 
 export const metadata: Metadata = {
   title: 'Schulfundraiser',
-  description: 'LFG',
+  description: 'School Project Mint',
+  metadataBase: new URL(NEXT_PUBLIC_URL),
   openGraph: {
     title: 'Schulfundraiser',
-    description: 'LFG',
+    description: 'School Project Mint',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
   other: {
@@ -44,10 +44,41 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page() {
+const Page: React.FC = () => {
   return (
     <>
       <h1>Schulfundraiser</h1>
+      <div>
+        <h2>Get to Know Us</h2>
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://youtube.com/embed/UgkxH_D2DdkYjKQB-qHir0AzL6NQL5OjqQkf?feature=shared" 
+          title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowFullScreen>
+        </iframe>
+      </div>
+      <div>
+        <h2>Mint Project</h2>
+        <p>
+          <a href="https://zora.co/collect/base:0xb1031d5f7e02de0481ec1c2743409fb6534401e9" target="_blank" rel="noopener noreferrer">
+            Visit Mint Project
+          </a>
+        </p>
+      </div>
+      <div>
+        <h2>Donation</h2>
+        <iframe 
+          src="https://fv.sophie-scholl-schule.eu/verein/wie-kann-ich-spenden" 
+          width="560" 
+          height="315" 
+          frameBorder="0">
+        </iframe>
+      </div>
     </>
   );
 }
+
+export default Page;
