8a790c66365a6d3bcc7b822340dca6f78e8533a2
GritnotGit <no.grit.no.pearl@icloud.com>
2024-05-26 08:52:25 +0200

    Update image URL to use edgapbridger-image.png

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
 import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from './config'; // Import the configuration
-import React from 'react';
+import { NEXT_PUBLIC_URL } from './config';
 
-// Frame metadata with customized buttons and links
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      action: 'link',
       label: 'Dev - Youth Matching',
+      action: 'link',
       target: 'https://developer-connect-vwxu15e.gamma.site/',
     },
     {
-      action: 'link',
       label: 'Call for Action',
+      action: 'link',
       target: 'https://developer-connect-vwxu15e.gamma.site/sponsor',
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/image.png`, // Ensure this points to the correct image URL
+    src: `${NEXT_PUBLIC_URL}/edgapbridger-1.png`,
     aspectRatio: '1:1',
   },
   input: {
@@ -27,52 +25,23 @@ const frameMetadata = getFrameMetadata({
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
-// Metadata for the page
 export const metadata: Metadata = {
   title: 'EdGapBridger',
   description: 'ETHBerlin04 Hackathon Project - EdGapBridger',
-  metadataBase: new URL(NEXT_PUBLIC_URL),
   openGraph: {
     title: 'EdGapBridger',
     description: 'ETHBerlin04 Hackathon Project - EdGapBridger',
-    images: [`${NEXT_PUBLIC_URL}public/image.png`], // Ensure this points to the correct image URL
+    images: [`${NEXT_PUBLIC_URL}/edgapbridger-1.png`],
   },
   other: {
     ...frameMetadata,
   },
 };
 
-const Page: React.FC = () => {
+export default function Page() {
   return (
-    <div>
+    <>
       <h1>EdGapBridger</h1>
-      <div>
-        <h2>Hackathon Concept</h2>
-        <p>
-          Learn more about our hackathon concept and how you can participate.
-          <a href="https://edgapbridger-2-day-hacka-06zz8ib.gamma.site/" target="_blank" rel="noopener noreferrer">
-            Visit the Hackathon Website
-          </a>
-        </p>
-      </div>
-      <div>
-        <h2>For Dev - Youth Matching</h2>
-        <p>
-          <a href="https://developer-connect-vwxu15e.gamma.site/" target="_blank" rel="noopener noreferrer">
-            Visit Dev - Youth Matching
-          </a>
-        </p>
-      </div>
-      <div>
-        <h2>Call for Action</h2>
-        <p>
-          <a href="https://developer-connect-vwxu15e.gamma.site/sponsor" target="_blank" rel="noopener noreferrer">
-            Call for Action
-          </a>
-        </p>
-      </div>
-    </div>
+    </>
   );
-};
-
-export default Page;
+}
